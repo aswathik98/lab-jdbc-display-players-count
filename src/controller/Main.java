16b2a7681d43b89d8943664e6aaa6227e06bed15
Aswathi Krishnan <66994935+aswathik98@users.noreply.github.com>
2020-07-30 17:56:54 +0530

    Add files via upload

--- src/controller/Main.java
+++ src/controller/Main.java
@@ -1,1 +1,30 @@
+package controller;
 
+import java.sql.Connection;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import dao.SkillDAO;
+import model.Skill;
+import utility.ConnectionManager;
+
+public class Main{
+	
+	public static void main(String[] args) throws Exception {
+		ConnectionManager cm = new ConnectionManager();
+		Connection conn = cm.getConnection();
+		
+SkillDAO skilldao = new SkillDAO();
+		
+		TreeMap<Integer,Skill> map = new TreeMap<Integer,Skill>();
+		map=skilldao.skillCount();
+		System.out.println("Skill"+"\t"+"Players");
+		
+		for(Entry<Integer,Skill> e: map.entrySet())
+		{
+			int  count=e.getKey();
+			Skill skill=e.getValue();
+			System.out.println(skill.getSkillName() + "\t" + count);
+		}	
+	}
+}
